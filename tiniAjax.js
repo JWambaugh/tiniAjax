601e9c70a16092340cda97fb369508404c0d44ba
Jordan Wambaugh <jordan@wambaugh.org>
2012-05-23 18:55:35 -0700

    updated read me, made lib even lighter

--- tiniAjax.js
+++ tiniAjax.js
@@ -1,5 +1,5 @@
 //Very light ajax lib that replaces jQuery. By Jordan Wambaugh
 var jQuery={};var $=jQuery;
-$.x=function(){try{return new ActiveXObject('Msxml2.XMLHTTP')}catch(e){try{return new ActiveXObject('Microsoft.XMLHTTP')}catch(e){return new XMLHttpRequest()}}};
-$.send=function(u,f,m,a){var x=$.x();x.open(m,u,true);x.onreadystatechange=function(){if(x.readyState==4){f(x.responseText)}};if(m=='POST')x.setRequestHeader('Content-type','application/x-www-form-urlencoded');x.send(a)};
-$.ajax=function(c){var p="";if(c.data!=undefined){for(var x in c.data){p+=escape(x)+"="+escape(c.data[x])+'&'}}if(c.type=='POST'){$.send(c.url,c.success,'POST',p)}else{if(c.url.indexOf('?')==-1)c.url+='?';else c.url+='&';$.send(c.url+p,c.success,'GET')}};
+$.r=function(){try{return new ActiveXObject('Msxml2.XMLHTTP')}catch(e){try{return new ActiveXObject('Microsoft.XMLHTTP')}catch(e){return new XMLHttpRequest()}}};
+$.s=function(u,f,m,a){var r=$.r();r.open(m,u,true);r.onreadystatechange=function(){if(r.readyState==4){f(r.responseText)}};if(m=='POST')r.setRequestHeader('Content-type','application/x-www-form-urlencoded');r.send(a)};
+$.ajax=function(c){var p="";if(c.data!=undefined){for(var x in c.data){p+=escape(x)+"="+escape(c.data[x])+'&'}}if(c.type=='POST'){$.s(c.url,c.success,'POST',p)}else{if(c.url.indexOf('?')==-1)c.url+='?';else c.url+='&';$.s(c.url+p,c.success,'GET')}};
